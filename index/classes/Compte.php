1331428a65e11b01c5d863407c59945df7b88a57
helene <darte.helene@gmail.com>
2025-02-03 16:44:20 +0100

    poo php

---
+++ index/classes/Compte.php
@@ -0,0 +1,17 @@
+<?php
+
+/* objet Compte Bancaire */
+
+class Compte
+{
+    /*Titulaire du compte
+     @var string */
+    public $titulaire;
+
+    /*Solde du compte 
+    @var float*/
+    public $solde;
+
+}
+
+?>
